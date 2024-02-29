978af760467fe034052c449394d613764511f6dc
Kolin Guo <ruguo@ucsd.edu>
2024-02-29 14:46:39 -0800

    Add realsense2_camera rs_launch.py into mycobot_280pi driver.launch.py

--- mycobot_280/mycobot_280pi/launch/driver.launch.py
+++ mycobot_280/mycobot_280pi/launch/driver.launch.py
@@ -1,7 +1,10 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 from pymycobot import PI_BAUD, PI_PORT
 
 
@@ -9,25 +12,35 @@ def generate_launch_description():
     # Declare arguments
     declared_arguments = []
     declared_arguments.append(
-        DeclareLaunchArgument("port", default_value=PI_PORT, description="MyCobot Port")
+        DeclareLaunchArgument("port", default_value=PI_PORT, description="MyCobot port")
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "baudrate", default_value=str(PI_BAUD), description="Baud rate"
+            "baudrate", default_value=str(PI_BAUD), description="MyCobot baud rate"
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
             "init_qpos",
             default_value=str([0.0] * 5 + [0.785398]),
-            description="Initial joint angles in radians",
+            description="MyCobot initial joint angles in radians",
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "publish_frequency", default_value="30", description="Publishing frequency"
+            "publish_frequency",
+            default_value="30",
+            description="MyCobot joint_states publishing frequency",
         )
     )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "with_camera",
+            default_value="False",
+            description="Whether or not to launch realsense2_camera node",
+        )
+    )
+
     # joint_state_publisher_node = Node(
     #     package="mycobot_280pi",
     #     executable="joint_state_publisher",
@@ -53,4 +66,14 @@ def generate_launch_description():
         ],
     )
 
-    return LaunchDescription(declared_arguments + [driver_node])
+    camera_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare("realsense2_camera"),
+                "launch/rs_launch.py",
+            ])
+        ),
+        condition=IfCondition(LaunchConfiguration("with_camera")),
+    )
+
+    return LaunchDescription(declared_arguments + [driver_node, camera_launch])
